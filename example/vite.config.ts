739449865af6dcc869c1775b5e4b341e9fd4e482
thednp <thednp@users.noreply.github.com>
2024-08-11 15:25:53 +0300

    Update cfg

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 import svg from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svg({
-    include: "**/*.svg?react",
-  })],
+  plugins: [
+    react(),
+    svg({
+      include: '**/*.svg?react',
+    }),
+  ],
   server: {
     port: 4000,
   },
