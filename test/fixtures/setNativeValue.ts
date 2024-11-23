650ac6dfa6b7652d83e1992fed1199b8ff06dd44
Dino <dnp.digital@gmail.com>
2024-11-23 21:29:16 +0200

    update ignore files

---
+++ test/fixtures/setNativeValue.ts
@@ -0,0 +1,11 @@
+export default function setNativeValue(element: HTMLElement, value: string) {
+    const valueSetter = Object.getOwnPropertyDescriptor(element, 'value')?.set;
+    const prototype = Object.getPrototypeOf(element);
+    const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value')?.set;
+
+    if (valueSetter && valueSetter !== prototypeValueSetter) {
+        prototypeValueSetter?.call(element, value);
+    } else {
+        valueSetter?.call(element, value);
+    }
+}
